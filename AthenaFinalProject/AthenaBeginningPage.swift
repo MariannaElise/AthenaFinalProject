9312448371d91cd7020b4195a09180f52b660f8c
Marianna <MariannaElise102@gmail.com>
2021-07-21 10:35:49 -0700

    Added the view controllers

---
+++ AthenaFinalProject/AthenaBeginningPage.swift
@@ -0,0 +1,31 @@
+//
+//  AthenaBeginningPage.swift
+//  AthenaFinalProject
+//
+//  Created by Marianna Rodriguez-Rivera on 7/20/21.
+//
+
+import UIKit
+
+class AthenaBeginningPage: UIViewController {
+    @IBAction func athenaLogo(_ sender: UIButton) {
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
